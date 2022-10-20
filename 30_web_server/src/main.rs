39036cbc7da81d94c98a87f9040fac0bb3f6ed27
Matthew Bastow <MattBast@users.noreply.github.com>
2022-10-20 20:39:01 +0100

    Added 404 response for unknown paths.

--- 30_web_server/src/main.rs
+++ 30_web_server/src/main.rs
@@ -23,8 +23,15 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {:#?}", http_request);
     
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let request_line = &http_request[0];
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 OK", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
